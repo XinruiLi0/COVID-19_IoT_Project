eca34ea3cb5d4b211d5fc48c4e3012faa7554c65
xinyuchen <nick.chenbca@gmail.com>
2020-11-03 17:45:35 -0500

    Update UI

---
+++ Web/Web/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
