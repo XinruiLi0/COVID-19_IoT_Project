af3a4ef89b0c78ab15585fc19ba1105a8bc77785
xinyuchen <nick.chenbca@gmail.com>
2020-11-22 16:43:24 -0500

    Update UI

---
+++ Web/Web/Controllers/UserPageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class UserPageController : Controller
+    {
+        public IActionResult Doctor()
+        {
+            return View();
+        }
+    }
+}
