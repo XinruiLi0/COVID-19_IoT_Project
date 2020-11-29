f19cf2e79fdf6a5827e14fa3d07061968490e026
Yisheng Li <yisheng.li@carleton.ca>
2020-11-29 15:44:52 -0500

    Added login and signup function

---
+++ Mobile_app/IVMSP/IVMSP/UIViewController_Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController_Extensions.swift
+//  IVMSP
+//
+//  Created by Yisheng Li on 2020/11/26.
+//
+
+import UIKit
+
+extension UIViewController{
+    @IBAction func unwind(_ segue: UIStoryboardSegue){}
+}
+
+
--- Mobile_app/IVMSP/IVMSP/ViewController.swift
+++ Mobile_app/IVMSP/IVMSP/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.hideKeyboardWhenTappedAround()
     }
 
-
+    
+  
 }
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
