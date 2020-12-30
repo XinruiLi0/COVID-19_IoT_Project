9619d31a9ffc89aa8bad474693a803eca3d01f33
xinyuchen <nick.chenbca@gmail.com>
2020-12-30 15:26:48 -0500

    Add cache for doctor information

--- Web/Web/Controllers/HomeController.cs
+++ Web/Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult Doctor()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
