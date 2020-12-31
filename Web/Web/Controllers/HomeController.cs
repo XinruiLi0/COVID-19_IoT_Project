3513c23d6815dc843405003c3b6b76342efbde06
xinyuchen <nick.chenbca@gmail.com>
2020-12-30 23:30:02 -0500

    Add guard view page and test cookie derive.

--- Web/Web/Controllers/HomeController.cs
+++ Web/Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult Guard()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
