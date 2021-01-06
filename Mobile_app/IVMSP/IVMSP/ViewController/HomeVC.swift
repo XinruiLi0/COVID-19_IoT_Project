47e5f9ea7a530983ea8336f310ddaf1b40cdc0c0
Yisheng Li <yisheng.li@carleton.ca>
2021-01-06 18:49:54 -0500

    Added NFC scan and send record function

--- Mobile_app/IVMSP/IVMSP/ViewController/HomeVC.swift
+++ Mobile_app/IVMSP/IVMSP/ViewController/HomeVC.swift
@@ -9,64 +9,94 @@ import UIKit
 import CoreNFC
 
 class HomeVC: UIViewController, NFCTagReaderSessionDelegate {
-    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
-        print("Session Begun")
-    }
-    
-    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
-        print("Error when luna")
-    }
-    
-    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
-        print("connect  ")
-    }
-    
-
-    
-
     
     @IBOutlet var NFCText: UITextView!
     
     @IBOutlet var scan: UIButton!
     
     var nfcSession: NFCTagReaderSession?
-    var word = "None"
+    
+    var userEmail = "1181536731@qq.com"
+    var deviceID = "None"
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        self.hideKeyboardWhenTappedAround()
+        
     }
+    
     @IBAction func scanButton(_ sender: Any) {
         self.nfcSession = NFCTagReaderSession(pollingOption: .iso14443, delegate: self)
-        self.nfcSession?.alertMessage = "Hold near NFC"
+        self.nfcSession?.alertMessage = "Please Hold Near NFC Tag"
         self.nfcSession?.begin()
     }
     
     
     
-    
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//    }
-//
-//
-//
-//    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
-//        print("errorrrrr")
-//    }
-//
-//    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
-//        var result = ""
-//
-//        for payload in messages[0].records{
-//            result += String.init(data: payload.payload.advanced(by: 3), encoding: .utf8) ?? "Format not supported"
-//        }
-//
-//        DispatchQueue.main.async {
-//            self.NFCText.text = result
-//        }
+    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
+        print("Session Begun")
     }
-
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
+        print(error)
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
+        print("Detected NFC tag")
+        
+        if tags.count > 1{
+            session.alertMessage = "More than one tag detected"
+            session.invalidate()
+        }
+        
+        let tag = tags.first!
+        session.connect(to: tag) {(error) in
+            if nil != error{
+                session.invalidate(errorMessage: "connection failed")
+            }
+            if case let .miFare(sTag) = tag {
+                let UID = sTag.identifier.map{ String(format: "%.2hhx", $0)}.joined()
+                print("UID:", UID)
+                session.alertMessage = "Checked in \n Thank you"
+                session.invalidate()
+                DispatchQueue.main.async {
+                    self.NFCText.text = UID
+                    self.deviceID = UID
+                    
+                    self.sendCheckInRecord(deviceID: self.deviceID, vistorEmail: self.userEmail)
+                }
+            }
+        }
+    }
+    
+    
+    func sendCheckInRecord(deviceID: String, vistorEmail: String) {
+        let parameters = ["deviceID": deviceID, "vistorEmail": vistorEmail]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            
+        }.resume()
+    }
+    
+    
+    
+}
