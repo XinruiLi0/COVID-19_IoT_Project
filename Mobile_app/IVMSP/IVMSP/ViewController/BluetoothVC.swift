b43fca4fb9773e904ca0ff9725ff89bf55cd2bfe
Yisheng Li <yisheng.li@carleton.ca>
2021-01-25 22:57:56 -0500

    Added Map, Bluetooth tab page

--- Mobile_app/IVMSP/IVMSP/ViewController/BluetoothVC.swift
+++ Mobile_app/IVMSP/IVMSP/ViewController/BluetoothVC.swift
@@ -6,12 +6,72 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class BluetoothVC: UIViewController {
-
+class BluetoothVC: UIViewController,CBCentralManagerDelegate, ObservableObject, UITableViewDelegate, UITableViewDataSource{
+ 
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var myState = ""
+    var devices:[String] = []
+    var centralManager: CBCentralManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            self.myState = "central.state is .unknown"
+            print("central.state is .unknown")
+        case .resetting:
+            self.myState = "central.state is .resetting"
+            print("central.state is .resetting")
+        case .unsupported:
+            self.myState = "central.state is .unsupported"
+            print("central.state is .unsupported")
+        case .unauthorized:
+            self.myState = "central.state is .unauthorized"
+            print("central.state is .unauthorized")
+        case .poweredOff:
+            self.myState = "central.state is .poweredOff"
+            print("central.state is .poweredOff")
+        case .poweredOn:
+            self.myState = "central.state is .poweredOn"
+            print("central.state is .poweredOn")
+            centralManager.scanForPeripherals(withServices: nil)
+            
+        default:
+            print("error")
+        }
+        
+    }
+    
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        // print("Peripheral Name: \(String(describing: peripheral.name))  RSSI: \(String(RSSI.doubleValue))")
+        print("\(peripheral)")
+        self.devices.append("\(peripheral)")
+        self.tableView.reloadData()
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return devices.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! selfSizingTableViewCell
+        cell.cellText.text = devices[indexPath.row]
+        
+        return cell
+    }
+    
     
 }
--- Mobile_app/IVMSP/IVMSP/ViewController/MapVC.swift
+++ Mobile_app/IVMSP/IVMSP/ViewController/MapVC.swift
@@ -6,12 +6,62 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
     
+        mapView.showsUserLocation = true
+        
     }
     
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            locationManager.stopUpdatingLocation()
+            render(location)
+        }
+        
+        let casesLocation1 = MKPointAnnotation()
+        casesLocation1.title = "1"
+        casesLocation1.coordinate = CLLocationCoordinate2D(latitude: 45.381449, longitude: -75.733208)
+        
+        let casesLocation2 = MKPointAnnotation()
+        casesLocation2.title = "2"
+        casesLocation2.coordinate = CLLocationCoordinate2D(latitude: 45.397754, longitude: -75.673615)
+        
+        let casesLocation3 = MKPointAnnotation()
+        casesLocation3.title = "3"
+        casesLocation3.coordinate = CLLocationCoordinate2D(latitude: 45.364116, longitude: -75.685741)
+        
+        let casesLocation4 = MKPointAnnotation()
+        casesLocation4.title = "5"
+        casesLocation4.coordinate = CLLocationCoordinate2D(latitude: 45.335040, longitude: -75.610077)
+        
+        
+        mapView.addAnnotations([casesLocation1, casesLocation2, casesLocation3, casesLocation4])
+    }
+    
+    func render(_ location: CLLocation){
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
 }
