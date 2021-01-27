3414b5b4f29c564701085b5b5bdfc9988e39e6b6
xinyuchen <nick.chenbca@gmail.com>
2021-01-26 22:12:41 -0500

    Implemenetd Address and longtitude and latitude function.

--- Web/Web/wwwroot/js/Signup.js
+++ Web/Web/wwwroot/js/Signup.js
@@ -16,6 +16,7 @@
         $(".doctor").attr("style", "display:none;");
         $(".guard").attr("style", "display:block;");
         $(".user").attr("style", "display:none;");
+       
     });
 });
 
@@ -32,7 +33,7 @@ function DoctorAjax() {
     $.ajax({
         type: "POST",
         dataType: "text",
-        url: "/Login/userRegister",
+        url: "/Login/doctorRegister",
         data: { "userName": $("#dname").val(), "userEmail": $("#exampleInputEmail1").val(), "userPassword": $("#password1").val(), "userRole": 3 },
         success: function (data) {
             alert(data);
@@ -44,7 +45,42 @@ function DoctorAjax() {
     })
 };
 
+function UserAjax() {
+    if ($("#exampleInputEmail3").val() == "" || $("#password3").val() == "" || $("#uname").val() == "" || $("#age").val() == "") {
+        alert("You leave email or password or username or age empty. Please fill in the blank to Register!");
+        return;
+    }
+    if (($("#conpassword3").val()) != ($("#password3").val())) {
+        alert("Your two password mismatch! Please do again!");
+        return;
+    }
+
+    var inf;
+    if ($("#radio1").prop("checked", true)) {
+        inf = 0;
+    }
+    if ($("#radio2").prop("checked", true)) {
+        inf = 1;
+    }
+    $.ajax({
+        type: "POST",
+        dataType: "text",
+        url: "/Login/userRegister",
+        data: { "userName": $("#uname").val(), "userEmail": $("#exampleInputEmail3").val(), "userPassword": $("#password3").val(), "age": $("#age").val(), "hasInfectedBefore": inf },
+        success: function (data) {
+            alert(data);
+            window.location.href = "/Login/Signup";
+        },
+        error: function (xhr, ts, et) {
+            alert("Error");
+        }
+    })
+};
+
+
 function GuardAjax() {
+    var geocoder = new google.maps.Geocoder();
+    var address = document.getElementById("Address").value;
     if ($("#exampleInputEmail2").val() == "" || $("#password2").val() == "" || $("#gname").val() == "") {
         alert("You leave email or password or username empty. Please fill in the blank to Register!");
         return;
@@ -54,59 +90,27 @@ function GuardAjax() {
         alert("Your two password mismatch! Please do again!");
         return;
     }
-
-    $.ajax({
-        type: "POST",
-        dataType: "text",
-        url: "/Login/userRegister",
-        data: { "userName": $("#gname").val(), "userEmail": $("#exampleInputEmail2").val(), "userPassword": $("#password2").val(), "userRole": 2 },
-        success: function (data) {
-            alert(data);
-            window.location.href = "/Login/Signup";
-        },
-        error: function (xhr, ts, et) {
-            alert("Error");
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var loca = [];
+            loca[0] = results[0].geometry.location.lat();
+            loca[1] = results[0].geometry.location.lng();          
+            $.ajax({
+                type: "POST",
+                dataType: "text",
+                url: "/Login/guardRegister",
+                data: { "guardName": $("#gname").val(), "guardEmail": $("#exampleInputEmail2").val(), "guardPassword": $("#password2").val(), "address": address, "latitude": loca[0], "longitude": loca[1] },
+                success: function (data) {
+                    alert(data);
+                    window.location.href = "/Login/Signup";
+                },
+                error: function (xhr, ts, et) {
+                    alert("Error");
+                }
+            })
+        } else {
+            alert("Request failed. Please input valid address!!!!!!!!!")
+            return;
         }
-    })
+    });
 };
-
-function UserAjax() {
-    if ($("#exampleInputEmail3").val() == "" || $("#password3").val() == "" || $("#uname").val() == "") {
-        alert("You leave email or password or username empty. Please fill in the blank to Register!");
-        return;
-    }
-    if (($("#conpassword3").val()) != ($("#password3").val())) {
-        alert("Your two password mismatch! Please do again!");
-        return;
-    }
-
-    $.ajax({
-        type: "POST",
-        dataType: "text",
-        url: "/Login/userRegister",
-        data: { "userName": $("#uname").val(), "userEmail": $("#exampleInputEmail3").val(), "userPassword": $("#password3").val(), "userRole": 1 },
-        success: function (data) {
-            alert(data);
-            window.location.href = "/Login/Signup";
-        },
-        error: function (xhr, ts, et) {
-            alert("Error");
-        }
-    })
-};
-
-function getLocation() {
-    var x;
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    }
-    else { x = "Geolocation is not supported by this browser."; }
-
-    return x;
-}
-
-function showPosition(position) {
-    var pos = new Array(position.coords.latitude, position.coords.longitude);
-    alert(pos);
-    GuardAjax();
-}
