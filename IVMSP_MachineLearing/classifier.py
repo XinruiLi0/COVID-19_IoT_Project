60bbb788fc2a15d8b182327e75f5e05538c9e34d
XinruiLi0 <xinruili3@cmail.carleton.ca>
2021-02-23 00:24:34 -0500

    refactor: Package the ML model into a class

--- IVMSP_MachineLearing/classifier.py
+++ IVMSP_MachineLearing/classifier.py
@@ -5,40 +5,71 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 import pyodbc
-conn = pyodbc.connect(
-    'Driver={SQL Server};'
-    'Server=ivmsdb.cs17etkshc9t.us-east-1.rds.amazonaws.com,1433;'
-    'Database=ivmsdb;'
-    'UID=admin;'
-    'PWD=ivmsdbadmin;'
-)
-cursor = conn.cursor()
-sql = "SELECT * from DataForML;"
+
+class MachineLearningModel(object):
+    conn = pyodbc.connect(
+        'Driver={SQL Server};'
+        'Server=ivmsdb.cs17etkshc9t.us-east-1.rds.amazonaws.com,1433;'
+        'Database=ivmsdb;'
+        'UID=admin;'
+        'PWD=ivmsdbadmin;'
+    )
+
+    model = LogisticRegression()
+
+    def __init__(self):
+        # Prepare training data
+        cursor = self.conn.cursor()
+        sql = "SELECT Age, HasInfectedBefore, Periods, CloseContact, ClosePeriods, Status from DataForML;"
+        data = pd.read_sql(sql,self.conn)
+
+        x = data[['Age','HasInfectedBefore','Periods','CloseContact','ClosePeriods']]
+        y = data["Status"]
+
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
+
+        # Initialize ML model
+        self.model.fit(x_train, y_train)
+
+    def pridict(self, age, hasInfectedBefore, periods, closeContact, closePeriods):
+        return self.model.predict(x_test)
+
+def main():
+    conn = pyodbc.connect(
+        'Driver={SQL Server};'
+        'Server=ivmsdb.cs17etkshc9t.us-east-1.rds.amazonaws.com,1433;'
+        'Database=ivmsdb;'
+        'UID=admin;'
+        'PWD=ivmsdbadmin;'
+    )
+
+    cursor = conn.cursor()
+    sql = "SELECT * from DataForML;"
+
+    data = pd.read_sql(sql,conn)
+    print(data)
+
+    from sklearn.linear_model import LogisticRegression
+    logreg = LogisticRegression()
+
+    x = data[['Age','HasInfectedBefore','Periods','CloseContact','ClosePeriods']]
+    y = data["Status"]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
+
+    print(x)
+    print(y)
+
+    model = LogisticRegression()
+    model.fit(x_train, y_train)
 
 
-data = pd.read_sql(sql,conn)
-print(data)
+    y_pred = model.predict(x_test)
+    print(y_pred)
+    print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(model.score(x_test, y_test)))
+    confusion_matrix = confusion_matrix(y_test, y_pred)
+    print(confusion_matrix)
 
-
-
-from sklearn.linear_model import LogisticRegression
-logreg = LogisticRegression()
-
-x = data[['Age','HasInfectedBefore','Periods','CloseContact','ClosePeriods']]
-y = data["Status"]
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
-
-print(x)
-print(y)
-
-model = LogisticRegression()
-model.fit(x_train, y_train)
-
-
-y_pred = model.predict(x_test)
-print(y_pred)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(model.score(x_test, y_test)))
-confusion_matrix = confusion_matrix(y_test, y_pred)
-print(confusion_matrix)
+if __name__ == "__main__":
+    main()
 
