223477def389c968d3dd1f6bec2d2ebdc47743ea
XinruiLi0 <xinruili3@cmail.carleton.ca>
2021-02-23 10:58:53 -0500

    update: Update predict function for one time prediction

--- IVMSP_MachineLearing/classifier.py
+++ IVMSP_MachineLearing/classifier.py
@@ -31,44 +31,11 @@ class MachineLearningModel(object):
         # Initialize ML model
         self.model.fit(x_train, y_train)
 
-    def pridict(self, age, hasInfectedBefore, periods, closeContact, closePeriods):
-        return self.model.predict(x_test)
+    def predict(self, age, hasInfectedBefore, periods, closeContact, closePeriods):
+        return self.model.predict([[age, hasInfectedBefore, periods, closeContact, closePeriods]])
 
 def main():
-    conn = pyodbc.connect(
-        'Driver={SQL Server};'
-        'Server=ivmsdb.cs17etkshc9t.us-east-1.rds.amazonaws.com,1433;'
-        'Database=ivmsdb;'
-        'UID=admin;'
-        'PWD=ivmsdbadmin;'
-    )
-
-    cursor = conn.cursor()
-    sql = "SELECT * from DataForML;"
-
-    data = pd.read_sql(sql,conn)
-    print(data)
-
-    from sklearn.linear_model import LogisticRegression
-    logreg = LogisticRegression()
-
-    x = data[['Age','HasInfectedBefore','Periods','CloseContact','ClosePeriods']]
-    y = data["Status"]
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
-
-    print(x)
-    print(y)
-
-    model = LogisticRegression()
-    model.fit(x_train, y_train)
-
-
-    y_pred = model.predict(x_test)
-    print(y_pred)
-    print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(model.score(x_test, y_test)))
-    confusion_matrix = confusion_matrix(y_test, y_pred)
-    print(confusion_matrix)
+    print(MachineLearningModel().predict(20, 1, 1000, 0, 0))
 
 if __name__ == "__main__":
     main()
